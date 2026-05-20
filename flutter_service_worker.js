58687153a5868b1f2fd34e017cdbd8c0d953bc26
muherbas <muherbasgyou@gmail.com>
2026-05-20 13:23:46 +0000

    Deploying to gh-pages from @ muherbas/blajar-fultter@ce0301e1b88e02810a6de6e8490b31ced72ec8ab 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"assets/fonts/MaterialIcons-Regular.otf": "e1483316512ca368e3408360472cab6e",
+"assets/fonts/MaterialIcons-Regular.otf": "9cb41b8b401341db06aca1a31c879aab",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
-"main.dart.js": "30db10df3e097975f2ff331e014778e6"};
+"main.dart.js": "6162d39289c12c4e2760c9d1d5a5274d"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
