b56c0ddc6f372a74cc2b19c9a5bc0028c046f7f9
muherbas <muherbasgyou@gmail.com>
2026-05-21 21:15:37 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
+      theme: ThemeData(useMaterial3: true, colSchemeSeed: Colors.indigo),
       home: const DashboardAtlet(),
     );
   }
