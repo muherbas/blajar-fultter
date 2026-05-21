5d3a3442fc8ff67f43ea0012947044a47fffb9dd
muherbas <muherbasgyou@gmail.com>
2026-05-21 16:30:48 +0000

    Deploying to gh-pages from @ muherbas/blajar-fultter@473893aa5d578f8ad1b10c76a6154f0387ec452c 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
-"assets/NOTICES": "e4e7cdcab44c927e291b6b32355f84be",
+"assets/NOTICES": "ff6d6ceac9eab0809bd833d560925ac8",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "version.json": "71fc56de6b6b932c17988afb52baca63",
 "index.html": "66c9212ef6a7a2933e280bc6e401173b",
