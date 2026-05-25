f1372ee2638b06bfd2e4f85d4c168890e7bb70e8
muherbas <muherbasgyou@gmail.com>
2026-05-25 15:06:16 +0000

    Deploying to gh-pages from @ muherbas/blajar-fultter@5e825d050e25efd35a73166728d7bc451d3c2589 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
-"main.dart.js": "bcfabce6587c353c7d1ea87f6660f038"};
+"main.dart.js": "c856fde5a74fb481e98a596f9c893acc"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
